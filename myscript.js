402f9a44a3fd5dd71b1a689045be21bf7e56ddb6
ran-jb <ranerlich@gmail.com>
2024-04-16 21:34:55 +0300

    loop and functions

--- myscript.js
+++ myscript.js
@@ -1,2 +1,5 @@
-let name = 'aaa'
-name.
+function sumThree(x, y, z) {
+  // console.log('This function sums the three numbers')
+  // console.log('the numbers i got are:', x, y, z)
+  console.log('the sum is:', x + y + z)
+}
